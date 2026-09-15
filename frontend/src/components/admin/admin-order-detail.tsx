04d52ae854302d5dc2d26ca5621934ebca8a096c
Alexander Markov <sasha.markov.mail@gmail.com>
2026-09-15 13:05:00 +0300

    feat(security): добавил XSS защиту (sanitize-html on backend, DOMPurify on frontend)

--- frontend/src/components/admin/admin-order-detail.tsx
+++ frontend/src/components/admin/admin-order-detail.tsx
@@ -13,6 +13,7 @@ import { ordersActions } from '../../services/slice/orders'
 import { getOrderByNumber } from '../../services/slice/orders/thunk'
 import { adapterOrderFromServer } from '../../utils/adapterOrderFromServer'
 import { Preloader } from '../preloader'
+import { sanitizeHtml } from '../../utils/sanitizeHtml'
 import styles from './admin.module.scss'
 
 const ActionsButton = () => {
@@ -103,7 +104,7 @@ export default function AdminOrderDetail() {
                     <>
                         <div
                             dangerouslySetInnerHTML={{
-                                __html: dataInfo.comment,
+                                __html: sanitizeHtml(dataInfo.comment),
                             }}
                         />
                     </>
